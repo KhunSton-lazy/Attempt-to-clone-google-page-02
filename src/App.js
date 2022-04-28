7dd83cc88ea592aed8b92bc3037caf8ee9e2300f
khun ston <cocacolamonster15@gmail.com>
2022-04-28 13:00:51 +0700

    figured out the parallax scrolling effect

--- src/App.js
+++ src/App.js
@@ -7,25 +7,42 @@ function App() {
    
   });
 
+  const google = useParallax({ 
+    translateY: ["0px", "550px"],
+    speed: -5
+  });
+
+  const antinomy = useParallax({ 
+    translateY: ["-300px", "500px"],
+    speed: -5
+  });
+
+  const daysOfType = useParallax({ 
+    translateY: ["-300px", "500px"],
+    speed: -5
+  });
+
+
+
   return (
     <>
       <Parallax>
-      <div ref={ref} className="parallax bg-fixed">
-        <h1 class="text-9xl text-white grid place-items-center pt-[25%]">Google</h1>
+        <div ref={ref} className="google h-screen bg-cover bg-center bg-fixed ">
+          <h1 ref={google.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Google</h1>
         </div>
       </Parallax>
       
     <Parallax>
-    <div ref={ref} className="parallax01">
-          <h1>Hello world lorem</h1>
+      <div ref={ref} className="antinomy h-screen bg-cover bg-center bg-fixed overflow-hidden ">
+       <h1 ref={antinomy.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Antinomy</h1>
       </div>
     </Parallax>
 
     <Parallax>
-      <div ref={ref} className="parallax" >
-        <h1>lorem</h1>
-        </div>
-      </Parallax>
+      <div ref={ref} className="daysOfType h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">36 days of type</h1>
+      </div>
+    </Parallax>
      
 
       
