63dec9f2ebf4ac4c49f8ac92605acada7d6804cc
khun ston <cocacolamonster15@gmail.com>
2022-04-28 14:20:56 +0700

    added correct image for hero page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   });
 
   const google = useParallax({ 
-    translateY: ["0px", "550px"],
+    translateY: ["0px", "500px"],
     speed: -5
   });
 
@@ -28,7 +28,7 @@ function App() {
     <>
       <Parallax>
         <div ref={ref} className="google h-screen bg-cover bg-center bg-fixed ">
-          <h1 ref={google.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Google</h1>
+          <h1 ref={google.ref} class="text-6xl md:text-9xl text-white text-center">Google</h1>
         </div>
       </Parallax>
       
@@ -44,8 +44,82 @@ function App() {
       </div>
     </Parallax>
      
+    {/* Ayco */}
+    <Parallax>
+      <div ref={ref} className="Ayco h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Ayco</h1>
+      </div>
+    </Parallax>
 
+    {/* L'Echonova */}
+    <Parallax>
+      <div ref={ref} className="LEchonova h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">L'Echonova</h1>
+      </div>
+    </Parallax>
+
+    {/* Lvmh */}
+    <Parallax>
+      <div ref={ref} className="Lvmh h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Lvmh</h1>
+      </div>
+    </Parallax>
+
+    {/* Hublot */}
+    <Parallax>
+      <div ref={ref} className="Hublot h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Hublot</h1>
+      </div>
+    </Parallax>
+
+    {/* Byu TV */}
+    <Parallax>
+      <div ref={ref} className="ByuTv h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Byu Tv</h1>
+      </div>
+    </Parallax>
       
+    {/* Scale Ai */}
+    <Parallax>
+      <div ref={ref} className="ScaleAi h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Scale Ai</h1>
+      </div>
+    </Parallax>
+
+    {/* Nuage-sur-Mer */}
+    <Parallax>
+      <div ref={ref} className="NuageSurMer h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Nuage-sur-Mer</h1>
+      </div>
+    </Parallax>
+
+    {/* 36 days of type */}
+    <Parallax>
+      <div ref={ref} className="DaysOfType02 h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">36 days of type</h1>
+      </div>
+    </Parallax>
+
+    {/* fast company magazine */}
+    <Parallax>
+      <div ref={ref} className="FastCompanyMagazine h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Fast Company Magazine</h1>
+      </div>
+    </Parallax>
+
+    {/* Tous a table */}
+    <Parallax>
+      <div ref={ref} className="TousATable h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Tous a table</h1>
+      </div>
+    </Parallax>
+
+    {/* Bic */}
+    <Parallax>
+      <div ref={ref} className="Bic h-screen bg-cover bg-center bg-fixed overflow-auto" >
+      <h1 ref={daysOfType.ref} class="text-6xl md:text-9xl text-white grid place-items-center">Bic</h1>
+      </div>
+    </Parallax>
 
     </>
       
